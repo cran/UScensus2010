538a790a6b02e4c355121595a3b8086396601c59
Jonathan Gilligan <jonathan.gilligan@vanderbilt.edu>
2015-05-11 16:36:53 -0500

    Fixed version check for R versions 3.0 and higher.

--- R/install.county.R
+++ R/install.county.R
@@ -6,10 +6,10 @@ if(x=="osx" | x=="linux"){install.packages("UScensus2010county", repos="http://l
 	}
 
 if(x=="windows"){
-	rVer<-as.numeric(R.Version()$minor)
-	
-	if(rVer>=11){
-		install.packages("UScensus2010county", repos="http://lakshmi.calit2.uci.edu/census2000/R/",type="source")
+  rVer<-c(major = as.numeric(R.Version()$major), minor = as.numeric(R.Version()$minor))
+  
+  if(rVer['major'] > 2 || (rVer['major'] == 2 && rVer['minor'] >=11)){
+    install.packages("UScensus2010county", repos="http://lakshmi.calit2.uci.edu/census2000/R/",type="source")
 		return()
 		}else{stop("Not Available Yet")}
 }
